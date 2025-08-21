68bbb15f3cd48f04d397c14a505adafe9978b55d
Артем Кривошей <artemkryvoshei@gmail.com>
2025-08-21 14:10:13 +0300

    init commit
    just prepare project for future work

---
+++ SmallMarketUnityProject/Assets/Core/EventBus/IEventBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.EventBus
+{
+    public interface IEventBus
+    {
+        void Subscribe<T>(Action<T> handler);
+        void Unsubscribe<T>(Action<T> handler);
+        void Publish<T>(T eventData);
+    }
+}
---
+++ SmallMarketUnityProject/Assets/Core/Other/ConstantsHolder.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Other
+{
+    public static class ConstantsHolder
+    {
+        public const string LEVEL_INDEX_KEY = "CURRENT_LEVEL_INDEX";
+        public const int LOAD_SCREEN_ADDITIONAL_WAIT = 3000;
+        public const float ENEMY_ANIMATOR_FLOAT_NUMBER = 0.01f;
+    }
+}
